ebb74416170b3d495ca0b1641cf1896ee30ed853
Hilton <hilton.kbr@gmail.com>
2019-03-10 23:28:57 -0300

    Inserção dos comentários e correção de erro

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,3 +1,6 @@
+//"Lab 1 - Questão 1" 
+//Created by Hilton e Lúcio 10/03/2019
+
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
@@ -6,12 +9,14 @@
 #include "perimetros.h"
 #include "volumes.h"
 
+//Declara o namespace a ser usado como default
 using namespace std;
 
+//Função que exibe as instruções de uso do programa.
 void instrucoes()
 {
 	cout << "FORMAS GENERICAS:" << endl;
-	cout << "# ./Questao1 triangulo base altura" << endl;
+	cout << "# ./Questao1 triangulo lado" << endl;
 	cout << "# ./Questao1 retangulo base altura" << endl;
 	cout << "# ./Questao1 quadrado lado" << endl;
 	cout << "# ./Questao1 circulo raio" << endl;
@@ -22,18 +27,16 @@ void instrucoes()
 
 }
 
+
 int main(int argc, char *argv[])
-{	if(argc == 1)
+{	
+	//Alerta que é exibido quando nenhum parâmetro é enviado.
+	if(argc == 1)
 	{
 		cout << "Nenhum parâmetro foi identificado, para efetuar o cálculo execute da seguinte forma:" << endl << endl;
 		instrucoes();
 	}
-	else if(argc < 2)
-	{
-		cout << "ERRO na passagem de parâmetros, para efetura o cálculo execute da seguinte forma" << endl << endl;
-		instrucoes();
-	}
-	
+	//Execução do cálculo do quadrado, checa se foi enviado o valor necessario para o cálculo.
 	else if(strcmp(argv[1], "triangulo") == 0 && argc == 3)
 	{
 		float lado;
@@ -42,7 +45,7 @@ int main(int argc, char *argv[])
 		cout << "area do triangulo: " << area_triang(lado) << endl;
 		cout << "perimetro do triangulo: " << perimetro_triang(lado) << endl;
 	}
-       	
+    //Execução do cálculo do retangulo, checa se foram enviados os 2 valores necessarios para o cálculo.
 	else if(strcmp(argv[1], "retangulo") == 0 && argc == 4)
 	{
 		float base, altura;
@@ -52,7 +55,7 @@ int main(int argc, char *argv[])
 		cout << "area do retangulo: " << area_retang(base, altura) << endl;
 		cout << "perimetro do retangulo: " << perimetro_retang(base, altura) << endl;
 	}
-	
+	//Execução do cálculo do quadrado, checa se foi enviado o valor necessario para o cálculo.
 	else if(strcmp(argv[1], "quadrado") == 0 && argc == 3)
 	{
 		float lado;
@@ -61,7 +64,7 @@ int main(int argc, char *argv[])
 		cout << "area do quadrado: " << area_quad(lado) << endl;
 		cout << "perimetro do quadrado: " << perimetro_quad(lado) << endl;
 	}
-	
+	//Execução do cálculo do circulo, checa se foi enviado o valor necessario para o cálculo.
 	else if(strcmp(argv[1], "circulo") == 0 && argc == 3)
 	{
 		float raio;
@@ -70,7 +73,7 @@ int main(int argc, char *argv[])
 		cout << "area do circulo: " << area_circ(raio) << endl;
 		cout << "perimetro do circulo: " << perimetro_circ(raio) << endl;
 	}
-
+	//Execução do cálculo do piramide, checa se foram enviados os 2 valores necessarios para o cálculo.
 	else if(strcmp(argv[1], "piramide") == 0 && argc == 4)
 	{
 		float lado_base, lateral;
@@ -80,7 +83,7 @@ int main(int argc, char *argv[])
 		cout << "area da piramide: " << area_piramide(lado_base, lateral) << endl;
 		cout << "volume da piramide: " << volume_piramide(lado_base, lateral) << endl;
 	}
-
+	//Execução do cálculo do cubo, checa se foi enviado o valor necessario para o cálculo.
 	else if(strcmp(argv[1], "cubo") == 0 && argc == 3)
 	{
 		float lado;
@@ -89,7 +92,7 @@ int main(int argc, char *argv[])
 		cout << "area do cubo: " << area_cubo(lado) << endl;
 		cout << "volume do cubo: " << volume_cubo(lado) << endl;
 	}
-
+	//Execução do cálculo do paralelepipedo, checa se foram enviados os 3 valores necessarios para o cálculo.
 	else if(strcmp(argv[1], "paralelepipedo") == 0 && argc == 5)
 	{
 		float lado1, lado2, lado3;
@@ -100,7 +103,7 @@ int main(int argc, char *argv[])
 		cout << "area do paralelepipedo: " << area_paralele(lado1, lado2, lado3) << endl;
 		cout << "volume do paralelepipedo: " << volume_paralele(lado1, lado2, lado3) << endl;
 	}
-
+	//Execução do cálculo do esfera, checa se foi enviado o valor necessario para o cálculo.
 	else if(strcmp(argv[1], "esfera") == 0 && argc == 3)
 	{
 		float raio;
@@ -109,7 +112,7 @@ int main(int argc, char *argv[])
 		cout << "area da esfera: " << area_esfera(raio) << endl;
 		cout << "volume da esfera: " << volume_esfera(raio) << endl;
 	}
-	
+	//Alerta que é exibido quando nenhum parâmetro incorreto é enviado.
 	else
 	{
 		cout << "ERRO na passagem de parametros!!!" << endl << endl;
